3a6f9446517eab0fb133a9aecab2eba8b62e912e
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-28 20:09:53 +0300

    Add files via upload

--- thu_python_18/client.py
+++ thu_python_18/client.py
@@ -4,6 +4,28 @@ import socket
 import threading
 import time
 
+# собираем сообщения с сервера
+def receving (name, sock, switch):
+    while not switch:
+        try:
+            while True:
+                data, addr = sock.recvfrom(1024)
+                print('\n' + data.decode('utf-8'))
+                time.sleep(0.2)
+        except:
+            pass
 
+# ваш порт и ip
+host = socket.gethostbyname(socket.gethostname())
+port = 0
 
+# сервера порт и ip
+server = ("192.168.31.210", 11719)
 
+# сокет
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host,port))
+s.setblocking(0)
+
+name = input('$ name: ')
+s.sendto(name.encode("utf-8"), server)
