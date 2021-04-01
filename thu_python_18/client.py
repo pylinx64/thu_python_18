2deb803dcfd074a37604f23f7c510bd0d53a7db5
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-01 22:14:03 +0300

    Add files via upload

--- thu_python_18/client.py
+++ thu_python_18/client.py
@@ -20,7 +20,7 @@ host = socket.gethostbyname(socket.gethostname())
 port = 0
 
 # сервера порт и ip
-server = ("192.168.31.210", 11719)
+server = ("192.168.31.154", 11719)
 
 # сокет
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,4 +28,24 @@ s.bind((host,port))
 s.setblocking(0)
 
 name = input('$ name: ')
-s.sendto(name.encode("utf-8"), server)
+# отправка на сервер
+name_1 = '[' + name + '] => join server'
+s.sendto(name_1.encode("utf-8"), server)
+
+done=False
+join = False
+rT = threading.Thread(target = receving, args = ("RecvThread", s, done))
+rT.start()
+
+# False == False -> True
+while done==False: 
+    try:
+        message=input('message -> ')
+        s.sendto(('['+name+'] => '+message).encode("utf-8"), server)
+    except:
+        name_2 =  '[' + name + '] <= left server'
+        s.sendto(name_2.encode("utf-8"), server)
+        done=True
+
+rT.join()
+s.close()
---
+++ thu_python_18/try-except.py
@@ -0,0 +1,14 @@
+try:
+    x = 10
+    print(x/0)
+except:
+    pass
+
+if x==10:
+    pass
+
+def myABC():
+    pass
+
+while True:
+    pass
